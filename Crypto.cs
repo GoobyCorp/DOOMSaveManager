c071bec40daa57e93a6f3b2645ec3e03eb7527be
John <pls@gooby.co>
2020-03-31 21:28:28 -0400

    Fixed Steam path bug and added in profile rehashing code

--- Crypto.cs
+++ Crypto.cs
@@ -11,7 +11,13 @@ namespace DOOMSaveManager
 {
     public static class Crypto
     {
-        public static byte[] EncryptAndDigest(string aad, byte[] data) {
+		private const uint C1 = 0xCC9E2D51;
+		private const uint C2 = 0x1B873593;
+		private const uint C3 = 0xE6546B64;
+
+		public static uint rotr(uint n, int d) => (n >> d) | (n << (32 - d));
+
+		public static byte[] EncryptAndDigest(string aad, byte[] data) {
             byte[] nonce = Utilities.RandomBytes(12);
             byte[] aadBytes = Encoding.UTF8.GetBytes(aad);
             byte[] aadHash = new SHA256Managed().ComputeHash(aadBytes);
@@ -45,5 +51,93 @@ namespace DOOMSaveManager
 
             return plaintext;
         }
-    }
+
+		public static byte[] CalcProfileHash(byte[] data) {
+			byte[] pfData = new byte[data.Length - 0xC];
+			Buffer.BlockCopy(data, 0xC, pfData, 0, pfData.Length);
+
+			return FarmHash_Hash32(pfData);
+		}
+
+		public static uint Mur(uint a, uint h) {
+			// Taken from https://github.com/google/farmhash/blob/master/src/farmhash.cc
+
+			a *= C1;
+			a = rotr(a, 17);
+			a *= C2;
+			h ^= a;
+			h = rotr(h, 19);
+
+			return h * 5 + C3;
+		}
+
+		public static byte[] FarmHash_Hash32(byte[] data) {
+			// Taken from https://github.com/google/farmhash/blob/master/src/farmhash.cc
+
+			uint size = (uint)data.Length;
+
+			uint h = size;
+			uint g = C1 * size;
+			uint f = g;
+
+			uint a0 = rotr(BitConverter.ToUInt32(data, (int)(size - 4)) * C1, 17) * C2;
+			uint a1 = rotr(BitConverter.ToUInt32(data, (int)(size - 8)) * C1, 17) * C2;
+			uint a2 = rotr(BitConverter.ToUInt32(data, (int)(size - 12)) * C1, 17) * C2;
+			uint a3 = rotr(BitConverter.ToUInt32(data, (int)(size - 16)) * C1, 17) * C2;
+			uint a4 = rotr(BitConverter.ToUInt32(data, (int)(size - 20)) * C1, 17) * C2;
+
+			h ^= a0;
+			h = rotr(h, 19);
+			h = h * 5 + C3;
+			h ^= a3;
+			h = rotr(h, 19);
+			h = h * 5 + C3;
+
+			g ^= a1;
+			g = rotr(g, 19);
+			g = g * 5 + C3;
+			g ^= a2;
+			g = rotr(g, 19);
+			g = g * 5 + C3;
+
+			f += a4;
+			f = rotr(f, 19) + 113;
+
+			uint iters = (size - 1) / 20;
+
+			uint s = 0;
+			do {
+				uint a = BitConverter.ToUInt32(data, (int)s);
+				uint b = BitConverter.ToUInt32(data, (int)s + 4);
+				uint c = BitConverter.ToUInt32(data, (int)s + 8);
+				uint d = BitConverter.ToUInt32(data, (int)s + 12);
+				uint e = BitConverter.ToUInt32(data, (int)s + 16);
+				h += a;
+				g += b;
+				f += c;
+				h = Mur(d, h) + e;
+				g = Mur(c, g) + a;
+				f = Mur(b + e * C1, f) + d;
+				f += g;
+				g += f;
+				s += 20;
+			} while (--iters != 0);
+
+			g = rotr(g, 11) * C1;
+			g = rotr(g, 17) * C1;
+			f = rotr(f, 11) * C1;
+			f = rotr(f, 17) * C1;
+			h = rotr(h + g, 19);
+			h = h * 5 + C3;
+			h = rotr(h, 17) * C1;
+			h = rotr(h + f, 19);
+			h = h * 5 + C3;
+			h = rotr(h, 17) * C1;
+
+			byte[] r = BitConverter.GetBytes(h);
+			Array.Reverse(r);
+
+			return r;
+		}
+	}
 }
--- DoomEternal.cs
+++ DoomEternal.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.IO;
 using System.Collections.Generic;
 using System.Reflection.Emit;
+using Org.BouncyCastle.Cms;
+using Org.BouncyCastle.Crypto.Engines;
 
 namespace DOOMSaveManager
 {
@@ -19,6 +21,7 @@ namespace DOOMSaveManager
 
         public static void EnumerateSaves() {
             Saves = new DoomEternalSavePathCollection();
+
             if (Directory.Exists(BnetSavePath)) {
                 Saves.Add(new DoomEternalSavePath("savegame.unencrypted", DoomEternalSavePlatform.BethesdaNet, false));
                 foreach (var single in Directory.GetDirectories(BnetSavePath, "*.*", SearchOption.TopDirectoryOnly)) {
@@ -26,6 +29,7 @@ namespace DOOMSaveManager
                         Saves.Add(new DoomEternalSavePath(Path.GetFileNameWithoutExtension(single), DoomEternalSavePlatform.BethesdaNet));
                 }
             }
+
             if (Directory.Exists(SteamSavePath)) {
                 foreach (var steamId3 in Directory.GetDirectories(SteamSavePath, "*.*", SearchOption.TopDirectoryOnly)) {
                     foreach (var single in Directory.GetDirectories(steamId3, "*.*", SearchOption.TopDirectoryOnly)) {
@@ -35,44 +39,5 @@ namespace DOOMSaveManager
                 }
             }
         }
-
-		public static byte[] FarmHash_Hash32(byte[] data) {
-			uint size = (uint)data.Length;
-			if (size > 0x18) {
-				uint v3 = 5 * (Utilities.rotl(0x1B873593 * Utilities.rotl(0xCC9E2D51 * BitConverter.ToUInt32(data, (int)(size - 0x10)), 0xF) ^ 5 * (Utilities.rotl(size ^ 0x1B873593 * Utilities.rotl(0xCC9E2D51 * BitConverter.ToUInt32(data, (int)(size - 4)), 0xF), 0xD) - 0x52250EC), 0xD) - 0x52250EC);
-				uint v4 = 5 * (Utilities.rotl(0x1B873593 * Utilities.rotl(0xCC9E2D51 * BitConverter.ToUInt32(data, (int)(size - 0xC)), 0xF) ^ 5 * (Utilities.rotl(0xCC9E2D51 * size ^ 0x1B873593 * Utilities.rotl(0xCC9E2D51 * BitConverter.ToUInt32(data, (int)(size - 8)), 0xF), 0xD) - 0x52250EC), 0xD) - 0x52250EC);
-
-				uint v5 = (size - 1) / 20;
-
-				uint v6 = Utilities.rotl(0xCC9E2D51 * size + 0x1B873593 * Utilities.rotl(0xCC9E2D51 * BitConverter.ToUInt32(data, (int)(size - 0x14)), 0xF), 0xD) + 0x71;
-
-				int offset = 8;
-				for (uint i = v5; i > 0; i--) {
-					uint v8 = BitConverter.ToUInt32(data, offset + 4);
-					uint v9 = BitConverter.ToUInt32(data, offset - 8);
-					uint v10 = BitConverter.ToUInt32(data, offset);
-					uint v11 = BitConverter.ToUInt32(data, offset + 8);
-					uint v12 = BitConverter.ToUInt32(data, offset - 4);
-					offset += 20;
-					uint v13 = Utilities.rotl((v9 + v3) ^ 0x1B873593 * Utilities.rotl(0xCC9E2D51 * v8, 0xF), 0xD);
-					v3 = v13 - 0x19AB949C + v11 + 4 * v13;
-					uint v14 = Utilities.rotl((v12 + v4) ^ 0x1B873593 * Utilities.rotl(0xCC9E2D51 * v10, 0xF), 0xD);
-					uint v15 = v14 - 0x19AB949C + v9 + 4 * v14;
-					uint v16 = Utilities.rotl((v10 + v6) ^ 0x1B873593 * Utilities.rotl(0x100193A1 * v11 - 0x3361D2AF * v12, 0xF), 0xD);
-					v6 = v15 + v16 + v8 + 4 * (v16 - 0x66AE527);
-					v4 = v6 + v15;
-				}
-
-				uint cksm = 0xCC9E2D51 * Utilities.rotl(5 * Utilities.rotl(0xCC9E2D51 * (Utilities.rotl(0xCC9E2D51 * Utilities.rotr(v6, 0xB), 0xF) + Utilities.rotl(5 * (Utilities.rotl(v3 - 0x3361D2AF * Utilities.rotl(0xCC9E2D51 * Utilities.rotr(v4, 0xB), 0xF), 0xD) - 0x52250EC), 0xF)), 0xD) - 0x19AB949C, 0xF);
-				byte[] cksmBytes = BitConverter.GetBytes(cksm);
-				Array.Reverse(cksmBytes);
-				return cksmBytes;
-			} else if (size > 0xC)
-				return null;
-			else if (size > 4)
-				return null;
-			else
-				return null;
-		}
 	}
 }
