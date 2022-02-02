259229f1a5d0bd0f126452599550e06893296243
John Dong <pls@gooby.co>
2022-02-01 20:53:43 -0500

    Removed 7zip dependency

--- BuildBinary.cmd
+++ BuildBinary.cmd
@@ -1,6 +1,5 @@
 @echo off
 pushd "%~dp0"
-REM "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\msbuild.exe" $Env:GITHUB_WORKSPACE\DOOMSaveManager\DOOMSaveManager.csproj /p:Configuration=Release /p:Platform=x64
 "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:AllowUnsafeBlocks=true /p:Configuration=Release /p:Platform="Any CPU"
 :exit
 popd
