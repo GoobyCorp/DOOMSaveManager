c00aa033fbe694f61168f0b062a2bc7b5bf0de3d
John Dong <pls@gooby.co>
2022-02-01 20:48:54 -0500

    Removed 7zip dependency

--- BuildArtifact.cmd
+++ BuildArtifact.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "bin\Release" -ArchiveFileName "SampleX86.zip" -Format Zip
+powershell Compress-7Zip "bin\Release" -ArchiveFileName "DOOMSaveManager.zip" -Format Zip
 :exit
 popd
 @echo on
