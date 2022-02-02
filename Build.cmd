8307030073a5ccc4292ad34de02e893d2006960d
John Dong <pls@gooby.co>
2022-02-01 19:55:23 -0500

    Updated workflow

--- Build.cmd
+++ Build.cmd
@@ -1,8 +1,7 @@
 @echo off
 pushd "%~dp0"
-if exist Release rd /s /q Release
 REM "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\msbuild.exe" $Env:GITHUB_WORKSPACE\DOOMSaveManager\DOOMSaveManager.csproj /p:Configuration=Release /p:Platform=x64
-"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\msbuild.exe" $Env:GITHUB_WORKSPACE\DOOMSaveManager\DOOMSaveManager.csproj /p:Configuration=Release /p:Platform=x86
+"C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\msbuild.exe" $Env:GITHUB_WORKSPACE\DOOMSaveManager.csproj /p:Configuration=Release /p:Platform=x86
 :exit
 popd
 @echo on
